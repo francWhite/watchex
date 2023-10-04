849500c00e3292b50cdc2093717e3438879135ad
Francisco Metzger <francisco.metzger@gmail.com>
2023-10-04 18:03:34 +0200

    feat: implement basic project evaluation and file watching of items which are copied to output

---
+++ src/Watchex/ProjectEvaluator.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Build.Evaluation;
+
+namespace Watchex;
+
+internal static class ProjectEvaluator
+{
+  public static IEnumerable<CopyFileInfo> EvaluateFilesCopiedToOutput(
+    string rootProjectPath)
+  {
+    var (projectCollection, rootProject) = LoadProjectCollection(rootProjectPath);
+
+    var copyToOutputItems = projectCollection.LoadedProjects
+      .SelectMany(p => p.Items.Where(CopyItemToOutputDirectory));
+
+    var outputDirectory = Path.Combine(rootProject.DirectoryPath, rootProject.GetPropertyValue("OutputPath"));
+
+    return copyToOutputItems
+      .Select(item => CreateOutputFileInfo(item, outputDirectory));
+  }
+
+  private static CopyFileInfo CreateOutputFileInfo(ProjectItem projectItem, string outputDirectory)
+  {
+    var sourcePath = Path.Combine(projectItem.Project.DirectoryPath, projectItem.EvaluatedInclude);
+    var outputPath = Path.Combine(outputDirectory, projectItem.EvaluatedInclude);
+    return new CopyFileInfo(new FileInfo(sourcePath), new FileInfo(outputPath));
+  }
+
+  private static (ProjectCollection, Project) LoadProjectCollection(string rootProjectPath)
+  {
+    var projectCollection = new ProjectCollection();
+    var rootProject = projectCollection.LoadProject(rootProjectPath);
+    LoadProjectsRecursive(projectCollection, rootProject);
+
+    return (projectCollection, rootProject);
+  }
+
+  private static void LoadProjectsRecursive(ProjectCollection projectCollection, Project project)
+  {
+    var directProjectReferences = project.GetItems("ProjectReference")
+      .Select(x => x.EvaluatedInclude)
+      .Select(p => Path.GetFullPath(p, project.DirectoryPath))
+      .Where(path => projectCollection.LoadedProjects.Any(p => p.FullPath == path) == false)
+      .Select(projectCollection.LoadProject)
+      .ToList();
+
+    directProjectReferences.ForEach(p => LoadProjectsRecursive(projectCollection, p));
+  }
+
+  private static bool CopyItemToOutputDirectory(ProjectItem item)
+  {
+    var metaData = item.Metadata.SingleOrDefault(m => m.Name == "CopyToOutputDirectory");
+    return metaData != null && metaData.EvaluatedValue != "Never";
+  }
+}
